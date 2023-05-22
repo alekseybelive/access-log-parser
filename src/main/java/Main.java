f76debc720260d649906539345eba07e81873962
Martynov Aleksey <git.user.seven37@yandex.ru>
2023-05-22 11:41:49 +0300

    number-experiments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: "+ text.length());
-
+        System.out.println("Введите первое число: ");
+        Integer number = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число: ");
+        Integer number1 = new Scanner(System.in).nextInt();
+        double quotient = (double) number / number1;
+        int sum = number + number1;
+        int difference = number - number1;
+        int composition = number * number1;
+        System.out.println("Сумма числе: " + sum);
+        System.out.println("Разность чисел: " + difference);
+        System.out.println("Произведение: " + composition);
+        System.out.println("Частное: " + quotient);
     }
 }
